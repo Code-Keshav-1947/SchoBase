8b1cf9ac66ca06fda4b5d209e4a363458d0b605e
Keshav <manudeepaksharma2012@gmail.com>
2026-07-23 17:43:18 +0530

    Changed the Dashboard and logo an also set hover to the cards

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e5qnw96kb85r0ju2qbk88tuca1bu**1s=98s*jg*kxkxq-e5-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
