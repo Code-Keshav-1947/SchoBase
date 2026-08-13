db0926db51a8489963c678a54a9b72fe254e280a
Keshav <manudeepaksharma2012@gmail.com>
2026-08-13 17:48:53 +0530

    Supabase production database connected

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -92,13 +92,19 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    ) if os.environ.get('DATABASE_URL') else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
