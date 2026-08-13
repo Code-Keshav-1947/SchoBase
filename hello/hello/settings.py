6eba9878b2d1789576d6d42e2ea91a61c920e126
Keshav <manudeepaksharma2012@gmail.com>
2026-08-13 17:57:59 +0530

    feat: update database configuration to support environment variable for DATABASE_URL

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -92,18 +92,24 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    ) if os.environ.get('DATABASE_URL') else {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Agar Render par DATABASE_URL set hai toh use configure karega, nahi toh local SQLite chalega
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
