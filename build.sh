2e532d563152ea72d3f125e55991966a4f489ef9
Keshav <manudeepaksharma2012@gmail.com>
2026-08-13 16:42:46 +0530

    feat: enhance build script with comments for clarity

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
+
+# 1. Dependencies install karein
 pip install -r requirements.txt
+
+# 2. Hello folder ke andar jayein jahan manage.py hai
+cd hello
+
+# 3. Django commands chalayein
 python manage.py collectstatic --no-input
 python manage.py migrate
