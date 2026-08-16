10a793a10d283ab31ddaaed33abebb6c348411d5
Keshav <manudeepaksharma2012@gmail.com>
2026-08-16 12:52:46 +0530

    feat: update settings for environment variables and add Procfile for deployment

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e5qnw96kb85r0ju2qbk88tuca1bu**1s=98s*jg*kxkxq-e5-c'
+import os
+import dj_database_url
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-e5qnw96kb85r0ju2qbk88tuca1bu**1s=98s*jg*kxkxq-e5-c')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('RAILWAY_STATIC_URL', '*'), 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [f"https://{os.environ.get('RAILWAY_STATIC_URL')}"]
 
 
 # Application definition
@@ -92,24 +97,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-import os
-import dj_database_url
 
-# Agar Render par DATABASE_URL set hai toh use configure karega, nahi toh local SQLite chalega
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
 
 
 
@@ -156,12 +151,14 @@ USE_TZ = False  # Agar yeh True hai, toh ise False kar dein
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # lOGIN, LOGOUT AND USER MODEL
 LOGIN_URL = '/acc/login/'
