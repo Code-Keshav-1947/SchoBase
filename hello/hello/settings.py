dc6a27246792c74a0e7f51ae01b5d7629786b15d
Keshav <manudeepaksharma2012@gmail.com>
2026-08-29 15:17:11 +0530

    feat: configure project settings including apps, middleware, database, and cloud storage

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e5qnw96kb85r0ju2qbk88tuca1bu**1s=98s*jg*kxkxq-e5-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['schobase.onrender.com', 'localhost']
 
 
 # Application definition
