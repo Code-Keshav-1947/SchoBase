2c9ade2125a5638ba7b05c7299185b31f2fa28af
Keshav <manudeepaksharma2012@gmail.com>
2026-09-02 20:22:46 +0530

    refactor: remove superuser creation script from build.sh

--- hello/build.sh
+++ hello/build.sh
@@ -14,14 +14,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 export DJANGO_SETTINGS_MODULE=hello.settings
-python -c "
-import django
-django.setup()
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='boss').exists():
-    User.objects.create_superuser('boss', 'boss@example.com', 'BossPass123')
-    print('Superuser boss created successfully!')
-else:
-    print('Superuser boss already exists.')
-"
+
